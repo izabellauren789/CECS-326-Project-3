44010e783427f80a360f8a35e5a295ea58dfe503
izabellauren789 <izabellauren789@gmail.com>
2023-10-21 17:24:20 -0700

    adding random actions

--- RoadController.java
+++ RoadController.java
@@ -3,6 +3,7 @@
  * RoadController.java
  */
 import java.util.concurrent.Semaphore;
+import java.util.Random;
 
 public class RoadController {
    // Your code here
@@ -12,11 +13,37 @@ public class RoadController {
       semaphore = new Semaphore(1);
    }
 
+   public enum VillagerAction {
+      DRINK_WINE,
+      EAT_DONUT,
+      READ_JJK,
+      PLAY_CARDS,
+      CRY
+   }
+
    public void useRoad(String villageName) {
       try {
-         System.out.println(villageName + " is traveling on the road.");
+         System.out.println(villageName + " has arrived at the road.");
          semaphore.acquire();
-         System.out.println(villageName + " is reading JJK.");
+         VillagerAction randomAction = getRandomAction();
+         switch (randomAction) {
+            case DRINK_WINE:
+               System.out.println(villageName + " is drinking wine while traveling the road.");
+               break;
+            case EAT_DONUT:
+               System.out.println(villageName + " is eating a donut while traveling the road.");
+               break;
+            case READ_JJK:
+               System.out.println(villageName + " is reading JJK while traveling the road.");
+               break;
+            case PLAY_CARDS:
+               System.out.println(villageName + " is playing cards while traveling the road.");
+               break;
+            case CRY:
+               System.out.println(villageName + " is crying while traveling the road.");
+               break;
+
+         }
       } catch (InterruptedException e) {
          Thread.currentThread().interrupt();
       }
@@ -25,6 +52,11 @@ public class RoadController {
    public void releaseRoad(String villageName) {
       System.out.println(villageName + " has finished the exchange.");
       semaphore.release();
+   }
+
+   private VillagerAction getRandomAction() {
+      Random random = new Random();
+      return VillagerAction.values()[random.nextInt(VillagerAction.values().length)];
    }
 
 }
